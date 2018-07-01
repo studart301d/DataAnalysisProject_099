fcf6e7d0adcd489809829bb7bf7a828e3c34d241
LAPTOP-JA0ID3ND\gabri <gabrielstudart1@hotmail.com>
2018-07-01 16:38:52 -0300

    falta só as exceções

--- MyTwitter.java
+++ MyTwitter.java
@@ -1,3 +1,7 @@
+package ProjectMyTwitter;
+
+import java.util.Vector;
+
 public class MyTwitter implements ITwitter{
 	private IRepositorioUsuario repositorio; //ONDE TIVER Vector<Perfil> tem que ser trocado por repositorio
 
@@ -35,7 +39,7 @@ public class MyTwitter implements ITwitter{
 				tweet.setUsuario(usuario);
 				tweet.setMensagem(mensagem);
 				perfil.addTweet(tweet);
-				Vector<Perfil> seguidores = perfil.getSeguidors();
+				Vector<Perfil> seguidores = perfil.getSeguidores();
 				for(int i = 0; i<seguidores.length();i++){
 					if(seguidores[0].isAtivo()){
 						seguidores[0].addTweet(mensagem);
@@ -75,6 +79,81 @@ public class MyTwitter implements ITwitter{
 					}
 				}
 				return tweets;
+			}else{
+				//exceção PDException
+			}
+		}else{
+			//exceção PIException
+		}
+	}
+	public void seguir(String seguidor,String seguido){
+		if(repositorio.buscar(seguido)!=null || repositorio.buscar(seguidor)!=null){
+			Perfil perfilSeguido = repositorio.buscar(seguido);
+			Perfil perfilSeguidor = repositorio.buscar(seguidor);
+			if(perfilSeguido.isAtivo() || perfilSeguidor.isAtivo()){
+				if(seguido != seguidor ){
+					perfilSeguido.addSeguidor(seguidor);
+					perfilSeguidor.addSeguido(seguido);
+				}else{
+					//exceção SIException
+				}
+			}else{
+				//exceção PDException
+			}
+		}else{
+			//exceção PIException
+		}
+	}
+	public int numeroSeguidores(String usuario){
+		if(repositorio.buscar(usuario)!=null){
+			Perfil perfil = repositorio.buscar(usuario);
+			if(perfil.isAtivo()){
+				Vector<Perfil> seguidores = uperfil.getSeguidores();
+				int quantidadeSeguidoresAtivo = 0;
+				for(int i = 0; i < seguidores.length(); i++) {
+					if(seguidores[i].isAtivo()) {
+						quantidadeSeguidoresAtivo++;
+					}
+				}
+				return quantidadeSeguidoresAtivo;
+			}else{
+				//exceção PDException
+			}
+		}else{
+			//exceção PIException
+		}
+	}
+	public Vector<Perfil> seguidores(String usuario){
+		if(repositorio.buscar(usuario)!=null){
+			Perfil perfil = repositorio.buscar(usuario);
+			if(perfil.isAtivo()){
+				Vector<Perfil> seguidores = uperfil.getSeguidores();
+				Vector<Perfil> seguidoresAtivo = new Vector<Perfil>;
+				for(int i = 0; i < seguidores.length(); i++) {
+					if(seguidores[i].isAtivo()) {
+						seguidoresAtivo[i] = seguidores[i]
+					}
+				}
+				return seguidoresAtivo;
+			}else{
+				//exceção PDException
+			}
+		}else{
+			//exceção PIException
+		}
+	}
+	public Vector<Perfil> seguidos(String usuario){
+		if(repositorio.buscar(usuario)!=null){
+			Perfil perfil = repositorio.buscar(usuario);
+			if(perfil.isAtivo()){
+				Vector<Perfil> seguidos = uperfil.getSeguidores();
+				Vector<Perfil> seguidosAtivo = new Vector<Perfil>;
+				for(int i = 0; i < seguidos.length(); i++) {
+					if(seguidos[i].isAtivo()) {
+						seguidosAtivo[i] = seguidos[i]
+					}
+				}
+				return seguidosAtivo;
 			}else{
 				//exceção PDException
 			}
