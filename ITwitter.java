1500e311d4c95b9d09af7c92205ae1ace6b17f45
LAPTOP-JA0ID3ND\gabri <gabrielstudart1@hotmail.com>
2018-07-01 14:46:27 -0300

    atualizando

--- ITwitter.java
+++ ITwitter.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 public interface ITwitter{
 	public void criarPerfil(Perfil usuario);
 	public void cancelarPerfil(String usuario);
-	public void tweetar(String usuario , String usuario);
+	public void tweetar(String usuario , String mensagem);
 	public Vector<Tweet> timeline(String usuario);
 	public Vector<Tweet> tweets(String usuario);
 	public void seguir(String seguidor,String seguido);
--- MyTwitter.java
+++ MyTwitter.java
@@ -4,4 +4,40 @@ public class MyTwitter implements ITwitter{
 	public MyTwitter(IRepositorioUsuario repositorio){
 		this.repositorio = repositorio;
 	}
+
+	public void criarPerfil(Perfil usuario){
+		if(Vector<Perfil>.buscar(usuario) == null){
+			Vector<Perfil>.cadastrar(usuario);
+		}else{
+			//exceção PEException
+		}
+	}
+
+	public void cancelarPerfil(String usuario){
+		if(Vector<Perfil>.buscar(usuario)!=null){
+			Perfil perfil = Vector<Perfil>.buscar(usuario);
+			if(perfil.isAtivo()){
+				perfil.setAtivo(false);
+				Vector<Perfil>.atualizar(perfil);
+			}else{
+				//exceção PDException
+			}
+		}else{
+			//exceção PIException
+		}
+	}
+
+	public void tweetar(String usuario , String mensagem){
+		if(Vector<Perfil>.buscar(usuario)!=null){
+			Perfil perfil = Perfil(Vector<Perfil>.buscar(usuario));
+			if(mensagem.length()>=1 && mensagem.length()<=140){
+				Tweet tweet;
+				tweet.setMensagem(mensagem);
+			}else{
+				//exceção MFPException
+			}	
+		}else{
+			//exceção PIException
+		}
+	}
 }
